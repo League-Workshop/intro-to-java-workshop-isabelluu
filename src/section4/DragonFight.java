b9f85b828fda762e705e198987bc7bcc2836fc98
isabelluu <league@WTS20.attlocal.net>
2018-06-29 11:41:10 -0700

    Day 5

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -30,13 +30,13 @@ String attack = JOptionPane.showInputDialog("Do you want to attack with a yell o
 		// 9. If they typed in "yell":
 if (attack.equals("yell")) {
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
-	int number = new Random().nextInt(0-10);		
+	int number = new Random().nextInt(10);		
 			//-- Subtract that number from the dragon's health variable 
 	dragonHealth-=number;		
 		// 10. If they typed in "kick":
 if (attack.equals("kick")) {		
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
-	int numbers = new Random().nextInt(0-25);		
+	int numbers = new Random().nextInt(25);		
 			//-- Subtract that number from the dragon's health variable
 	dragonHealth -= numbers;		
 			
@@ -47,14 +47,14 @@ if (attack.equals("kick")) {
 	playerHealth-= rnumber;	
 		
 		// 13. If the user's health is less than or equal to 0
-if (playerHealth <=0) {JOptionPane.showMessageDialog(null, "You lost. :(");
+if (playerHealth <=0) {JOptionPane.showMessageDialog(null, "You lost. :(");}
 		
 			//-- Tell the user that they lost
 			
 		
 		// 14. Else if the dragon's health is less than or equal to 0
-if (dragonHealth<=0) {JOptionPane.showMessageDialog(null, "The Dragon is dead and you now have a ton of gold!");	
-else 
+if (dragonHealth<=0) {JOptionPane.showMessageDialog(null, "The Dragon is dead and you now have a ton of gold!");}	
+else { JOptionPane.showMessageDialog(null, "The dragon's health is" + dragonHealth + "Your health is " + playerHealth);
 }
 			//-- Tell the user that the dragon is dead and they took a ton of gold!
 			
@@ -62,5 +62,5 @@ else
 			
 			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
 			
-		}
+		}}}}
 	
